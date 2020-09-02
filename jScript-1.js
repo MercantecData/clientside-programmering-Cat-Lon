4a953e73bf13b8e8a1ab3a6c9e356d72b1acd69e
william <william_kammer@live.dk>
2020-09-02 11:24:37 +0200

    hurtig push med mindre ændringer

--- jScript-1.js
+++ jScript-1.js
@@ -1,15 +1,20 @@
 var url = "https://api.openweathermap.org/data/2.5/weather?q=Viborg&appid=1d471668edec21b6fe58ad100710764d"
 var promise
 var jPromise
+var userInput
 
 function FetchWeatherData()
 {
-    if(document.getElementById('textBox')!= undefined){
-    url =  "https://api.openweathermap.org/data/2.5/weather?q=" + document.getElementById('textBox').value + "&appid=1d471668edec21b6fe58ad100710764d"}
+    userInput = document.getElementById('textBox').value
+    if(document.getElementById('textBox')!= undefined)
+    {
+        url =  "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=1d471668edec21b6fe58ad100710764d"
+    }
     promise = fetch(url)
-    .then(data => data.text())
+    .then(data=> data.text())
     .then(data=>jPromise = JSON.parse(data))
     .then(data=> PrintWeatherData())
+    .then (data=> setTimeout(FetchWeatherData, 900))
 }
 
 function PrintWeatherData()
